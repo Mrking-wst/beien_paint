fdb7f1ed4bc8a3c0e5f419411d167e7583ffde51
MrKing <2013888975@qq.com>
2025-09-02 14:43:16 +0800

    完善plc通信节点,断线间隔重连,数据正常读取

--- src/pkg_plc_communicate_cpp/src/pkg_plc_communicate_cpp/plc_communicate_node.cpp
+++ src/pkg_plc_communicate_cpp/src/pkg_plc_communicate_cpp/plc_communicate_node.cpp
@@ -16,15 +16,15 @@ namespace PlcCommunicate
     PlcCommunicateNode::PlcCommunicateNode(const std::string &nodeName)
         : Node(nodeName), is_connected_(false)
     {
-        this->declare_parameter<std::string>("plc_ip", "192.168.0.10");
+        this->declare_parameter<std::string>("plc_ip", "192.168.100.5");
         this->declare_parameter<int>("plc_port", 502);
         this->declare_parameter<int>("read_interval", 100);
-        this->declare_parameter<int>("reconnect_interval", 2000);
+        this->declare_parameter<int>("reconnect_interval", 5000);
         this->declare_parameter<int>("read_holding_register_start_address", 0);
         this->declare_parameter<int>("read_holding_register_num", 10);
         this->declare_parameter<int>("write_holding_register_start_address", 0);
         this->declare_parameter<int>("write_holding_register_num", 20);
-        this->declare_parameter<int>("response_timeout", 1000);
+        this->declare_parameter<int>("response_timeout", 1);
 
         this->get_parameter<std::string>("plc_ip", plc_ip_);
         this->get_parameter<int>("plc_port", plc_port_);
@@ -36,7 +36,7 @@ namespace PlcCommunicate
         this->get_parameter<int>("write_holding_register_num", write_holding_register_num_);
         this->get_parameter<int>("response_timeout", response_timeout_);
 
-        RCLCPP_INFO(this->get_logger(), "%s 节点初始化", this->get_name());
+        RCLCPP_INFO(this->get_logger(), "节点初始化");
 
         this->source_data_pub_ = this->create_publisher<UInt16MultiArray>("source_data_pub", 10);
         this->source_data_sub_ = this->create_subscription<UInt16MultiArray>("source_data_sub",
@@ -56,7 +56,7 @@ namespace PlcCommunicate
     {
         if (this->ctx_ == nullptr)
         {
-            RCLCPP_INFO(this->get_logger(), "[%s] modbus 已经被释放", this->get_name());
+            RCLCPP_INFO(this->get_logger(), "modbus 已经被释放");
             return;
         }
 
@@ -77,36 +77,38 @@ namespace PlcCommunicate
             modbus_close(this->ctx_);
             modbus_free(this->ctx_);
             this->ctx_ = nullptr;
-            RCLCPP_INFO(this->get_logger(), "[%s] 当前连接不为空，连接之前需要将之前连接清空释放.", this->get_name());
+            RCLCPP_INFO(this->get_logger(), "当前连接不为空，连接之前需要将之前连接清空释放.");
         }
 
         this->ctx_ = modbus_new_tcp(this->plc_ip_.c_str(), this->plc_port_);
         if (this->ctx_ == nullptr)
         {
             RCLCPP_INFO(this->get_logger(),
-                        " [%s] 无法使用 %s:%i", this->get_name(), this->plc_ip_.c_str(), this->plc_port_);
+                        "无法使用 %s:%i", this->plc_ip_.c_str(), this->plc_port_);
             return;
         }
 
+        RCLCPP_INFO(this->get_logger(), "tcp 实例创建成功：%s:%d", this->plc_ip_.c_str(), this->plc_port_);
         modbus_set_response_timeout(this->ctx_, this->response_timeout_, 0);
 
         if (modbus_connect(this->ctx_) == -1)
         {
             int err = errno;
             RCLCPP_INFO(this->get_logger(),
-                        "[%s] %s:%i，连接失败-%s",
-                        this->get_name(),
+                        "%s:%i，连接失败-%s",
                         this->plc_ip_.c_str(),
                         this->plc_port_,
                         modbus_strerror(err));
             modbus_free(this->ctx_);
             this->ctx_ = nullptr;
+            this->is_connected_ = false;
             return;
         }
 
         this->is_connected_ = true;
-        RCLCPP_INFO(this->get_logger(), "[%s] 成功连接modbus服务端：%s:%d", this->get_name(), this->plc_ip_.c_str(), this->plc_port_);
+        RCLCPP_INFO(this->get_logger(), "成功连接modbus服务端：%s:%d", this->plc_ip_.c_str(), this->plc_port_);
         this->reconnect_time_->cancel();
+        this->poll_time_->reset();
     }
 
     void
@@ -121,14 +123,16 @@ namespace PlcCommunicate
         if (count == -1)
         {
             int err = errno;
-            RCLCPP_INFO(get_logger(), "[%s] 参数:%d %d", this->get_name(), this->read_holding_register_start_address_, this->read_holding_register_num_);
-            RCLCPP_INFO(get_logger(), "[%s] 读取数据失败:%s", this->get_name(), modbus_strerror(err));
+            RCLCPP_INFO(get_logger(), "参数:%d %d", this->read_holding_register_start_address_, this->read_holding_register_num_);
+            RCLCPP_INFO(get_logger(), "读取数据失败:%s", modbus_strerror(err));
             this->is_connected_ = false;
             this->ConnectModbus(); //  重新连接一次
 
             if (!this->is_connected_)
             {
+                this->poll_time_->cancel();
                 this->reconnect_time_->reset(); //  启动周期性冲连
+                RCLCPP_INFO(this->get_logger(), "进入断线重连...");
             }
             return;
         }
@@ -148,8 +152,7 @@ namespace PlcCommunicate
         this->source_data_pub_->publish(msg);
 
         RCLCPP_INFO(this->get_logger(),
-                    "[%s] 发布数据：%s",
-                    this->get_name(),
+                    "发布数据：%s",
                     datas.str().c_str());
     }
 
@@ -159,8 +162,7 @@ namespace PlcCommunicate
         if (!this->is_connected_)
         {
             RCLCPP_INFO(this->get_logger(),
-                        "[%s] 正在重新连接：%s:%d...",
-                        this->get_name(),
+                        "正在重新连接：%s:%d...",
                         this->plc_ip_.c_str(),
                         this->plc_port_);
             this->ConnectModbus();
@@ -172,13 +174,13 @@ namespace PlcCommunicate
     {
         if (source_data.data.empty())
         {
-            RCLCPP_INFO(this->get_logger(), "[%s] 待写入的数据为空！", this->get_name());
+            RCLCPP_INFO(this->get_logger(), "待写入的数据为空！");
             return;
         }
 
         if (this->is_connected_)
         {
-            RCLCPP_INFO(this->get_logger(), "[%s] 远程服务端未连接，数据写入失败！", this->get_name());
+            RCLCPP_INFO(this->get_logger(), "远程服务端未连接，数据写入失败！");
             return;
         }
 
