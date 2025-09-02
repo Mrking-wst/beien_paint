52ac45fcbee73aa77e5bd0ec743c69185e487638
MrKing <2013888975@qq.com>
2025-09-02 15:07:29 +0800

    修复参数不能从config配置参数读取的问题

--- src/pkg_plc_communicate_cpp/launch/plc.launch.py
+++ src/pkg_plc_communicate_cpp/launch/plc.launch.py
@@ -1,8 +1,17 @@
 from launch import LaunchDescription
 from launch.actions import GroupAction
 from launch_ros.actions import Node,PushRosNamespace
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+
+    config = os.path.join(
+        get_package_share_directory('pkg_plc_communicate_cpp'),
+        'config',
+        'plc_params.yaml'
+    )
+
     return LaunchDescription([
         GroupAction(
             actions=[
@@ -12,7 +21,7 @@ def generate_launch_description():
                     executable='plc_communicate',
                     name='plc_communicate',
                     output='screen',
-                    parameters=['config/plc_params.yaml']
+                    parameters=[config]
                 )
         ])
     ])
